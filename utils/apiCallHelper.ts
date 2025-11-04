a46ec38505cf6a9779aa9935b238e90adf791e2a
Arty-Z <arty.zhuria@gmail.com>
2025-11-03 19:22:19 -0600

    link changed

--- utils/apiCallHelper.ts
+++ utils/apiCallHelper.ts
@@ -1,6 +1,7 @@
 import axios, { AxiosResponse } from 'axios';
 
-const API_BASE_URL = process.env.API_BASE_URL || 'https://automationexercise.com/api';
+const BASE_URL = process.env.BASE_URL || 'https://automationexercise.com';
+const API_BASE_URL = `${BASE_URL}/api`;
 
 interface APIResponse {
   status: number;
